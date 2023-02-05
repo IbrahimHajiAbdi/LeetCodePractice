bd2640f5dd2062f2c5913a957867db05c78e3ac5
IbrahimHajiAbdi <ibrahim.hajiabdi09@gmail.com>
2023-02-05 18:58:40 +0000

    1

--- Arrays & Hashing/242 Valid Anagram.py
+++ Arrays & Hashing/242 Valid Anagram.py
@@ -1,8 +1,10 @@
 def isAnagram(s, t):
-    if s == "" or t == "":
+    if len(s) != len(t):
         return False
+    
     map1 = {}
     map2 = {}
+    
     for i in s:
         if i not in map1:
             map1[i] = 1
@@ -10,7 +12,7 @@ def isAnagram(s, t):
             map1[i] += 1
     
     for i in t:
-            if i not in map2:
+        if i not in map2:
             map2[i] = 1
         else:
             map2[i] += 1
