8c0df214ef0c813df170c21302c6e42d2a33d511
IbrahimHajiAbdi <ibrahim.hajiabdi09@gmail.com>
2023-02-06 15:29:02 +0000

    updated valid anagram

--- Arrays & Hashing/242 Valid Anagram.py
+++ Arrays & Hashing/242 Valid Anagram.py
@@ -2,23 +2,19 @@ def isAnagram(s, t):
     if len(s) != len(t):
         return False
     
-    map1 = {}
-    map2 = {}
-    
-    for i in s:
-        if i not in map1:
-            map1[i] = 1
-        else:
-            map1[i] += 1
-    
-    for i in t:
-        if i not in map2:
-            map2[i] = 1
-        else:
-            map2[i] += 1
+    map1 = getHashmapOfLetters(s)
+    map2 = getHashmapOfLetters(t)
     
     return map1 == map2
 
+def getHashmapOfLetters(s):
+    map = {}
+    for i in s:
+        if i not in map:
+            map[i] = 1
+        else:
+            map[i] += 1
+    return map
+
 isAnagram("cat", "cat")
 
-    
