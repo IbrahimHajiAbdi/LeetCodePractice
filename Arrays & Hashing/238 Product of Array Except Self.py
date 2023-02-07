4ab601b15cc80a686d2e4557da613c015d05b591
IbrahimHajiAbdi <ibrahim.hajiabdi09@gmail.com>
2023-02-07 12:41:29 +0000

    changed 238

--- Arrays & Hashing/238 Product of Array Except Self.py
+++ Arrays & Hashing/238 Product of Array Except Self.py
@@ -2,7 +2,8 @@ def productExceptSelf(nums):
     map = {}
     for i in range(len(nums)):
         map[nums[i]] = map.get(nums[i], 0) + 1
-    
+    for i in range(len(nums)):
+        print(map.keys())
 
     print(map)
 
