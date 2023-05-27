8c244e3ee072e4157f6c5a4c9f0cac16d3cbe2bb
Ibrahim Haji Abdi <ibrahim.hajiabdi09@gmail.com>
2023-05-27 14:39:02 +0100

    added 238 problem

--- Arrays & Hashing/238 Product of Array Except Self.py
+++ Arrays & Hashing/238 Product of Array Except Self.py
@@ -1,11 +1,18 @@
 def productExceptSelf(nums):
-    map = {}
+    num_dict = {}
+    for num in nums:
+        if num in num_dict:
+            num_dict[num] = num_dict[num] + 1
+        else:
+            num_dict[num] = 1 
+    
+    ans = []
+    prod = 1
     for i in range(len(nums)):
-        map[nums[i]] = map.get(nums[i], 0) + 1
-    for i in range(len(nums)):
-        print(map.keys())
-
-    print(map)
-
+        for key, value in num_dict.items():
+            if nums[i] == key and value > 0:
+                value = value - 1
+            prod = prod * value * key
+            print(prod)
 
 productExceptSelf([1,2,3,4])
