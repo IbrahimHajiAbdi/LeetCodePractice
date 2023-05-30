4e85b67bbf2f6772dad304105051d545013f9e03
IbrahimHajiAbdi <ibrahim.hajiabdi09@gmail.com>
2023-05-30 19:24:33 +0100

    added 128

--- Arrays & Hashing/238 Product of Array Except Self.py
+++ Arrays & Hashing/238 Product of Array Except Self.py
@@ -1,18 +1,9 @@
 def productExceptSelf(nums):
-    num_dict = {}
-    for num in nums:
-        if num in num_dict:
-            num_dict[num] = num_dict[num] + 1
-        else:
-            num_dict[num] = 1 
+    total = 1
+    for i, num in enumerate(nums):
+        total = total * num
     
-    ans = []
-    prod = 1
     for i in range(len(nums)):
-        for key, value in num_dict.items():
-            if nums[i] == key and value > 0:
-                value = value - 1
-            prod = prod * value * key
-            print(prod)
-
+        
+        
 productExceptSelf([1,2,3,4])
