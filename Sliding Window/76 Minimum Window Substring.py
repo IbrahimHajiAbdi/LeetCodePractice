593f64d25909dbb6f5f82c31bc8ee362bb027c2f
Ibrahim Haji Abdi <ec21621@qmul.ac.uk>
2023-09-26 20:40:13 +0100

    added 76

--- Sliding Window/76 Minimum Window Substring.py
+++ Sliding Window/76 Minimum Window Substring.py
@@ -1,1 +1,8 @@
 def minWindow(s: str, t: str) -> str:
+    count = {}
+    t_count = {}
+    for char in t:
+        t_count[char] = 1 + t_count.get(char, 0)
+    l = 0
+    for r in range(len(s)):
+
