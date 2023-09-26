bb5efc5e008c3dcd3ecf6f3f04363572c1b8f56a
IbrahimHajiAbdi <ibrahim.hajiabdi09@gmail.com>
2023-09-26 20:16:26 +0100

    worked on 76

--- Sliding Window/76 Minimum Window Substring.py
+++ Sliding Window/76 Minimum Window Substring.py
@@ -1,8 +1,24 @@
 def minWindow(s: str, t: str) -> str:
     count = {}
     t_count = {}
+    res = ""
     for char in t:
         t_count[char] = 1 + t_count.get(char, 0)
     l = 0
+
+    def tIns():
+        for k, v in t_count.values():
+            if count.get(k) is not None:
+                if count.get(k) >= v:
+                    continue
+                else:
+                    return False
+            else:
+                return False
+        return True
+
     for r in range(len(s)):
+        count[r] = 1 + count.get(count[r], 0)
+        if tIns():
+            if
 
