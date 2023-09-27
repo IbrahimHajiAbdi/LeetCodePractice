f34b2b34c0ce4a8fdcf4fd6973110902bbf7c80d
Ibrahim Haji Abdi <ec21621@qmul.ac.uk>
2023-09-27 13:19:07 +0100

    finshed 76

--- Sliding Window/76 Minimum Window Substring.py
+++ Sliding Window/76 Minimum Window Substring.py
@@ -1,14 +1,21 @@
+#!/bin/python3.10
+
+# r pointer goes increments until there is a valid substring, then l pointer increments until the string is invalid
+# once the current substring is invalid, r pointer goes increments until it is valid again.
+# Once r pointer reaches the end of the string, the l pointer increments toward the end of the string.
 def minWindow(s: str, t: str) -> str:
+    if t == s:
+        return s
     count = {}
     t_count = {}
+    substring = ""
     res = ""
     for char in t:
         t_count[char] = 1 + t_count.get(char, 0)
-    l = 0
 
-    def tIns():
-        for k, v in t_count.values():
-            if count.get(k) is not None:
+    def checkValidSubstring() -> bool:
+        for k, v in t_count.items():
+            if count.get(k, 0) > 0:
                 if count.get(k) >= v:
                     continue
                 else:
@@ -17,8 +24,44 @@ def minWindow(s: str, t: str) -> str:
                 return False
         return True
 
-    for r in range(len(s)):
-        count[r] = 1 + count.get(count[r], 0)
-        if tIns():
-            if
+    l = 0
+    r = 0
+    while r < len(s) and l < len(s):
+        if checkValidSubstring():
+            if res != "":
+                if len(res) > len(substring):
+                    res = substring
+                count[s[l]] -= 1
+                l += 1
+                substring = substring[1:]
+            else:
+                res = substring
+                count[s[l]] -= 1
+                l += 1
+                substring = substring[1:]
+        else:
+            count[s[r]] = 1 + count.get(s[r], 0)
+            substring += s[r]
+            r += 1
+        if r >= len(s):
+            while l < len(s):
+                if checkValidSubstring():
+                    if res != "":
+                        if len(res) > len(substring):
+                            res = substring
+                    else:
+                        res = substring
+                count[s[l]] -= 1
+                l += 1
+                substring = substring[1:]
+                print(f"Substring: {substring}\nres: {res}\n")
+                print(f"l: {l}\nr: {r}\n")
+
+        print(f"Substring: {substring}\nres: {res}\n")
+        print(f"l: {l}\nr: {r}\n")
+    return res
+
+
+
+print(minWindow("ab", "b"))
 
